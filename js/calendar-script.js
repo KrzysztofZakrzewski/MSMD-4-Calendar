84235ad740f13060a7d0dcc3493e314e85f13c15
Krzysztof Zakrzewski <krzysztof.zakrzewski@protonmail.com>
2023-03-15 22:02:47 +0100

    retrieving an elements using destructuring assignment

---
+++ js/calendar-script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const calendar = document.querySelector('.calendar'),
+	date = document.querySelector('.date'),
+	daysContainer = document.querySelector('.days'),
+	prev = document.querySelector('.prev'),
+	next = document.querySelector('.next');
