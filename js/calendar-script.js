26b01f1aa86d93e17ae002c10003a8ac281454f5
Krzysztof Zakrzewski <krzysztof.zakrzewski@protonmail.com>
2023-03-15 22:12:45 +0100

    created date let's and month arrey

--- js/calendar-script.js
+++ js/calendar-script.js
@@ -1,7 +1,28 @@
 'use strict';
 
+// destructuring assignment
 const calendar = document.querySelector('.calendar'),
 	date = document.querySelector('.date'),
 	daysContainer = document.querySelector('.days'),
 	prev = document.querySelector('.prev'),
 	next = document.querySelector('.next');
+
+let today = new Date();
+let activeDay;
+let month = today.getFullMonth();
+let year = today.getFullYear();
+
+const months = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December',
+];
