2377357eed9d3e75e5d833f6d5aa6390092286f8
Krzysztof Zakrzewski <krzysztof.zakrzewski@protonmail.com>
2023-03-18 14:30:25 +0100

    added days in JS

--- js/calendar-script.js
+++ js/calendar-script.js
@@ -34,13 +34,51 @@ function initCalendar() {
 	const firstDay = new Date(year, month, 1);
 	const lastDay = new Date(year, month + 1, 0);
 	const prevLastDay = new Date(year, month, 0);
-	const precDays = prevLastDay.getDate();
+	const prevDays = prevLastDay.getDate();
 	const lastDate = lastDay.getDate();
-	const day = firstDay.getDate();
+	const day = firstDay.getDay();
 	const nextDays = 7 - lastDay.getDay() - 1;
 
 	// update date top of calendar
-	date.innerHTML = `${months[month]} ${year}`//months[month] + ' ' + year;
-}
+	date.innerHTML = `${months[month]} ${year}`; //months[month] + ' ' + year;
+
+	// adding days on dom
+
+	let days = '';
+
+	// prev month days
+
+	for (let i = day; i > 0; i--) {
+		days += `<div class="day prev-date">${prevDays - i + 1}</div>`;
+	}
+
+	
+	// current month days
+	
+	for (let i = 1; i <= lastDate; i++) {
+		// if day is today add clas today
+		
+		if (
+			i === new Date().getDate() &&
+			year === new Date().getFullYear() &&
+			month === new Date().getMonth()
+			) {
+				days += `<div class="day today">${i}</div>`;
+			}
+			
+			// add remaing as it is
+			else {
+				days += `<div class="day ">${i}</div>`;
+			}
+		}
+		
+		// nezt month days
+
+		for (let i =1; i<= nextDays; i++) {
+			days += `<div class="day next-date">${i}</div>`
+		}
+		
+		daysContainer.innerHTML = days;
+	}
 
 initCalendar();
