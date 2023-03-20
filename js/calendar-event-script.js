aab291f066b425892b80a2cf3513fa8fcdd089f1
Krzysztof Zakrzewski <krzysztof.zakrzewski@protonmail.com>
2023-03-20 15:22:05 +0100

    fuctionality for open/close AddEvent, and close AddEvent by click anywhere except AddEvent

--- js/calendar-event-script.js
+++ js/calendar-event-script.js
@@ -1,2 +1,23 @@
 'use strict';
 
+const addEventBtn = document.querySelector('.add-event'),
+	addEventContainer = document.querySelector('.add-event-wrapper'),
+	addEventCloseBtn = document.querySelector('.close'),
+	addEventTitle = document.querySelector('.event-name'),
+	addEventFrom = document.querySelector('.event-time-from'),
+	addEventTo = document.querySelector('.event-time-to');
+
+addEventBtn.addEventListener('click', () => {
+	addEventContainer.classList.toggle('active');
+});
+
+addEventCloseBtn.addEventListener('click', () => {
+	addEventContainer.classList.remove('active');
+});
+
+// close AddEvent "popup"
+document.addEventListener('click', (e) => {
+	if (e.target !== addEventBtn && !addEventContainer.contains(e.target)) {
+		addEventContainer.classList.remove('active');
+	}
+});
