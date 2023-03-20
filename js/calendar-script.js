6cb7d8d77e5cb22c88842b95e04ce2782b4f2a6b
Krzysztof Zakrzewski <krzysztof.zakrzewski@protonmail.com>
2023-03-20 20:30:30 +0100

    function for update events when a day is active

--- js/calendar-script.js
+++ js/calendar-script.js
@@ -338,6 +338,36 @@ function getActiveDay(date) {
 	eventDate.innerHTML = date + ' ' + months[month] + ' ' + year;
 }
 
+
+// function for update events when a day is active
+function updateEvents(date) {
+	let events = '';
+	eventsArr.forEach((event) => {
+		if (
+			date === event.day &&
+			month + 1 === event.month &&
+			year === event.year
+		) {
+			event.events.forEach((event) => {
+				events += `<div class="event">
+			  <div class="title">
+				<i class="fas fa-circle"></i>
+				<h3 class="event-title">${event.title}</h3>
+			  </div>
+			  <div class="event-time">
+				<span class="event-time">${event.time}</span>
+			  </div>
+		  </div>`;
+			});
+		}
+	});
+	// if the is no events
+	if (events === "") {
+		events = `<div class="no-event">
+				<h3>No Events</h3>
+			</div>`;
+	  }
+}
 // EventLISTENER
 
 prev.addEventListener('click', prevMonth);
