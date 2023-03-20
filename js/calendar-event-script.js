0e10af20f63572ba2b39da2019108726365b279f
Krzysztof Zakrzewski <krzysztof.zakrzewski@protonmail.com>
2023-03-20 16:29:28 +0100

    set time format for 'time-from' and 'time-to', alow ony 50 chars in title

--- js/calendar-event-script.js
+++ js/calendar-event-script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// import {date} from ".calendar-event-script.js"
+
 const addEventBtn = document.querySelector('.add-event'),
 	addEventContainer = document.querySelector('.add-event-wrapper'),
 	addEventCloseBtn = document.querySelector('.close'),
@@ -13,11 +15,43 @@ addEventBtn.addEventListener('click', () => {
 
 addEventCloseBtn.addEventListener('click', () => {
 	addEventContainer.classList.remove('active');
+	// addEventTitle.value  = '';
 });
 
 // close AddEvent "popup"
 document.addEventListener('click', (e) => {
 	if (e.target !== addEventBtn && !addEventContainer.contains(e.target)) {
 		addEventContainer.classList.remove('active');
+	}
+});
+
+// alow ony 50 chars in title
+addEventTitle.addEventListener('input', (e) => {
+	addEventTitle.value = addEventTitle.value.slice(0, 50);
+});
+
+// time format in "from" and "to" time
+
+addEventFrom.addEventListener('input', (e) => {
+	// remove anythingelse
+	addEventFrom.value = addEventFrom.value.replace(/[^0-9:]/g, '');
+	// Adding ":" after 2 numbers
+	if (addEventFrom.value.length === 2) {
+		addEventFrom.value += ':';
+	}
+	if (addEventFrom.value.length > 5) {
+		addEventFrom.value = addEventFrom.value.slice(0, 5);
+	}
+});
+
+addEventTo.addEventListener('input', (e) => {
+	// remove anythingelse
+	addEventTo.value = addEventTo.value.replace(/[^0-9:]/g, '');
+	// Adding ":" after 2 numbers
+	if (addEventTo.value.length === 2) {
+		addEventTo.value += ':';
+	}
+	if (addEventTo.value.length > 5) {
+		addEventTo.value = addEventFrom.value.slice(0, 5);
 	}
 });
